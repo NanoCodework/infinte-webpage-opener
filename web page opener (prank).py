2faef55de0bb4d25de2d7951f8b2acd5f46be460
ZatroTrox <spranavkrishna@gmail.com>
2022-02-20 20:06:39 +0530

    see

--- web page opener (prank).py
+++ web page opener (prank).py
@@ -2,6 +2,7 @@ import random
 import time
 import webbrowser
 while True:
+    #any site can be add for reference iam using youtube.coms
     sites = random.choice(["youtube.com"])
     visit = "https://{}".format(sites)
     webbrowser . open(visit)
